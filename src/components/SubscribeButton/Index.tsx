486a7c876a41677bdfaf818ae27c501759ee7794
wbrunovieira <wbrunovieira77@gmail.com>
2021-10-02 11:08:52 +0100

    created checkout stripe

--- src/components/SubscribeButton/Index.tsx
+++ src/components/SubscribeButton/Index.tsx
@@ -1,20 +1,50 @@
-import styles from './styles.module.scss';
+import { useSession, signIn } from 'next-auth/client';
+import { useRouter } from 'next/router';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
-  priceId: string
+  priceId: string;
 }
 
-export function SubscribeButton({priceId}: SubscribeButtonProps) {
-    
- 
-      
-    return (
-      <button
-        type="button"
-        className={styles.subscribeButton}
-        
-      >
-        Subscribe now
-      </button>
-    );
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+  const router = useRouter();
+
+  async function handleSubscribe() {
+    console.log(session)
+    if (!session) {
+      signIn('github')
+      return;
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts');
+      return;
+    }
+
+    //criação da checkout session
+    try {
+      const response = await api.post('subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message);
+    }
   }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={() => handleSubscribe()}
+    >
+      Subscribe now
+    </button>
+  );
+}
